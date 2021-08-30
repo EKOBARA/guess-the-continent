c9ed19fe84255039c66112f40844e6a3805f7b8c
ekobara <emmanuelkobara@gmail.com>
2021-08-30 13:23:41 -0400

    Questions polutate and answers are logged

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Keep track of all asked questions and their answers
 const results = {};
-
+const answers = [];
 
 /*----- cached element references -----*/
-let form = document.querySelector('form');
-let qNumber = document.querySelector('h3');
-let question = document.querySelector('h5');
-let play = document.getElementById('play');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const qNumber = document.querySelector('h3');
+const question = document.querySelector('h5');
+// const play = document.getElementById('play');
 
 /*----- event listeners -----*/
 
@@ -25,31 +26,51 @@ play.addEventListener('click', (event) => {
     .then(res => {
         
         let country = Math.floor(Math.random()*250);
-        console.log(res[country]);
+        // console.log(res[country]);
         qNumber.innerText = `On what continent is ${res[country].name} located?`;
         results[res[country].name] = res[country].region;
         qNumber.innerText = `Question ${num}`;
         question.innerText = `On what continent is ${res[country].name} located?`;
         play.style.display = 'none';
-        console.log(results)
+        // console.log(results)
     });
 });
        
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
 	// console.log(event);
-    qNumber.innerText = `Question ${num++}`;
+    qNumber.innerText = `Question ${num+=1}`;
 	const url = 'https://restcountries.eu/rest/v2/all';
 	fetch(url)
 		.then((res) => res.json())
 		.then((res) => {
             let country = Math.floor(Math.random() * 250);
-			console.log(res[country]);
-			question.innerText = `On what continent is ${res[country].name} located?`;
-            results[res[country].name] = res[country].region;
-			
+			// console.log(res[country]);
+            // let go = true;
+            // while (go){
+                if (num <= 10){
+			        question.innerText = `On what continent is ${res[country].name} located?`;
+                    results[res[country].name] = res[country].region;
+                    answers.push(input.value);
+                    console.log(results);
+                    console.log(answers);
+                    input.value = '';
+                } else {
+                    
+                    let li = document.createElement('li');
+                    let span = document.createElement('span');
+					let button = document.createElement('button');
+
+
+                    document.body.display = 'none';
+
+                    
+                }
+            
 		});
+
 });
+console.log(answers);
 // complaints.innerHTML = ''
 
     //     res.forEach((complaint) => {
